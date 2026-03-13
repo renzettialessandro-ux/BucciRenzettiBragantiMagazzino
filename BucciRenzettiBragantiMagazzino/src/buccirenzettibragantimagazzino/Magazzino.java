b8c8f144f30a2ce606f548b9092f4f07784d484d
buccialex <bucci.alex@franchettisalviani.net>
2026-03-13 12:05:28 +0100

    creata classe magazzino

---
+++ BucciRenzettiBragantiMagazzino/src/buccirenzettibragantimagazzino/Magazzino.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buccirenzettibragantimagazzino;
+import java.util.*;
+/**
+ *
+ * @author bucci.alex
+ */
+public class Magazzino {
+    /**
+     * attributi
+     */
+    private ArrayList<Prodotto> listaProdotti;
+    
+}
