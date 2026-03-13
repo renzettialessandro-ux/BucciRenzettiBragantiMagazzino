12111aed80eccd9c2008157dbfde1b4146c667fc
renzettialessandro-ux <renzetti.alessandro@franchettisalviani.net>
2026-03-13 12:04:07 +0100

    classe prodotto

---
+++ BucciRenzettiBragantiMagazzino/src/buccirenzettibragantimagazzino/Prodotto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buccirenzettibragantimagazzino;
+
+/**
+ *
+ * @author renzetti.alessandro
+ */
+public class Prodotto {
+   private String proId;
+   private String proNome;
+   private double proPrezzoAcq;
+   private double proPrezzovendite;
+   private int proScorta;
+   private int proScortaMin;
+   private int proVenduti;
+   
+}
